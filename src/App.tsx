6488830c90686963d552c18eb4c8c0e5886e6d56
Bhanu-Prakash-S <bhanuprakash.simhadri@gmail.com>
2025-06-20 22:35:05 +0530

    set up supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { supabase } from "./supabase";
+import { useEffect, useState } from "react";
+import type { Database } from "./lib/database.types";
+
+type Song = Database["public"]["Tables"]["songs_database"]["Row"];
+
 function App() {
-  return <h1> Hey! Welcome to the library</h1>;
+  const [songs, setSongs] = useState<Song[]>([]);
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
+  async function fetchSongs() {
+    let { data, error } = await supabase.from("songs_database").select("*");
+
+    if (error) throw error;
+    setSongs(data ?? []);
+  }
+
+  return (
+    <ul>
+      {songs.map((song) => (
+        <li key={song.id}> {song.song_name + " - " + song.category}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
